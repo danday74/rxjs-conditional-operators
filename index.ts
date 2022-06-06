20689dca5586f1e3a0023e25ee801517d2494fa8
danday74 <daniellewis777@gmail.com>
2022-06-06 17:18:12 +0100

    1 file modified

--- index.ts
+++ index.ts
@@ -1,6 +1,4 @@
-import { timer, take, pipe, map } from 'rxjs'
-
-const condition = false
+import { timer, take, pipe, map, mergeMap, iif, of } from 'rxjs'
 
 const double = () => {
   return pipe(
@@ -16,7 +14,30 @@ const triple = () => {
 
 timer(0, 1000).pipe(
   take(4),
-  condition ? double() : triple()
+  mergeMap(x => iif(() => x <= 1, of(x).pipe(double()), of(x).pipe(triple()))),
 ).subscribe(x => {
   console.log('output', x)
 })
+
+// import { timer, take, pipe, map } from 'rxjs'
+
+// const condition = false
+
+// const double = () => {
+//   return pipe(
+//     map((x: number) => x * 2)
+//   )
+// } 
+
+// const triple = () => {
+//   return pipe(
+//     map((x: number) => x * 3)
+//   )
+// }
+
+// timer(0, 1000).pipe(
+//   take(4),
+//   condition ? double() : triple()
+// ).subscribe(x => {
+//   console.log('output', x)
+// })
